db964e08eae1b324f569304ed1e092f4821d1996
suebtas <suebtas@Suebtass-Mac-Pro.local>
2015-03-23 23:58:34 +0700

    first

--- textShoot2/GameScene.swift
+++ textShoot2/GameScene.swift
@@ -9,37 +9,55 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+    private var levelNumber: UInt
+    private var playerLives: Int
+    private var finished = false
+    class func scene(size:CGSize, levelNumber:UInt) -> GameScene {
+        return GameScene(size: size, levelNumber: levelNumber)
     }
-    
+    init(size:CGSize, levelNumber:UInt) {
+        self.levelNumber = levelNumber
+        self.playerLives = 5
+        super.init(size: size)
+        backgroundColor = SKColor.whiteColor()
+        let lives = SKLabelNode(fontNamed: "Courier")
+        lives.fontSize = 16
+        lives.fontColor = SKColor.blackColor()
+        lives.name = "LivesLabel"
+        lives.text = "Lives: \(playerLives)"
+        lives.verticalAlignmentMode = .Top
+        lives.horizontalAlignmentMode = .Right
+        lives.position = CGPointMake(frame.size.width,
+        frame.size.height)
+        addChild(lives)
+        let level = SKLabelNode(fontNamed: "Courier")
+        level.fontSize = 16
+        level.fontColor = SKColor.blackColor()
+        level.name = "LevelLabel"
+        level.text = "Level \(levelNumber)"
+        level.verticalAlignmentMode = .Top
+        level.horizontalAlignmentMode = .Left
+        level.position = CGPointMake(0, frame.height)
+        addChild(level)
+    }
+    override convenience init(size:CGSize) {
+        self.init(size: size, levelNumber: 1)
+    }
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
-        
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
+        let location = touch.locationInNode(self)
+        } }
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    required init?(coder aDecoder: NSCoder) {
+        levelNumber = UInt(aDecoder.decodeIntegerForKey("level"))
+        playerLives = aDecoder.decodeIntegerForKey("playerLives")
+        super.init(coder: aDecoder)
+    }
+    override func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeInteger(Int(levelNumber), forKey: "level")
+        aCoder.encodeInteger(playerLives, forKey: "playerLives")
     }
 }
